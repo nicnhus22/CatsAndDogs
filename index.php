4de9bf3322d79c6a05ac669a53d7c7be1bcc8031
Nicolas <nicolas.husser@gmail.com>
2015-03-14 14:21:08 -0400

    Including giles

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE HTML>
+
+<html>
+
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>Your Website</title>
+</head>
+
+<body>
+
+	<!-- Header -->
+	<header>
+		<nav>
+			<ul>
+				<li>Your menu</li>
+			</ul>
+		</nav>
+	</header>
+	
+	<!-- Content -->	
+	<section>
+	
+		
+		
+	</section>
+
+	<!-- Footer -->
+	<footer>
+		<p>Copyright 2009 Your name</p>
+	</footer>
+
+</body>
+
+</html>

1bead53d912ec8207f6d78920591096f43638100
Nicolas <nicolas.husser@gmail.com>
2015-03-14 14:29:04 -0400

    Basic structure

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<title>Your Website</title>
+
+	<link rel="stylesheet" type="text/css" href="/public/css/style.css">
 </head>
 
 <body>
